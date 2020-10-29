5eb68b64a95550b56d2dc887ca06e000c316c373
Jeremy Adams <jeremy.adams@ga4gh.org>
2020-10-29 13:55:44 -0400

    gh-pages module handles both fetching gh-pages branch, and pushing new docs back to remote branch

--- src/lib/environment.js
+++ src/lib/environment.js
@@ -1,16 +1,19 @@
 import { merge } from 'lodash';
 import origin from 'remote-origin-url';
 import getRepoInfo from 'git-repo-info';
+import shell from 'shelljs';
 /*eslint no-process-env:0*/
 
 var repoInfo = getRepoInfo();
 repoInfo.branch = repoInfo.branch || process.env.TRAVIS_BRANCH || "undefined";
 
 const env = process.env.NODE_ENV;
+const wd = shell.pwd().toString();
 const repoOrigin = origin.sync();
 const environment = merge(repoInfo, {
     env,
-    repoOrigin
+    repoOrigin,
+    wd,
 });
 
 export default environment;
--- src/lib/gh-pages.js
+++ src/lib/gh-pages.js
@@ -1,5 +1,5 @@
 'use strict';
-import shell from 'shelljs';
+import path from 'path';
 import config from './config';
 import Log from './log';
 import fs from 'fs';
@@ -7,50 +7,70 @@ import subprocess from './subprocess';
 
 const log = new Log();
 
+class LocalGithubPagesBranch {
+
+    constructor() {
+        this.branchName = "gh-pages"
+        this.tempDir = path.join(config.wd, ".ghpages-tmp");
+        this.docFiles = [
+            config.docsRoot,
+            config.branchPathBase,
+            "openapi.json",
+            "openapi.yaml",
+        ];
+    }
+
+    createEnterTempDir() {
+        subprocess.removeDirs(this.tempDir);
+        subprocess.makeDirs(this.tempDir);
+        subprocess.changeDir(this.tempDir);
+        log.log(`\nCloning '${this.branchName}' branch into '${this.tempDir}'`);
+        subprocess.exec(`git clone --depth=1 --branch=${this.branchName} ${config.repoOrigin} .`);
+    }
+
+    leaveRemoveTempDir() {
+        subprocess.changeDir(config.wd);
+        subprocess.removeDirs(this.tempDir);
+    }
+
+    copyDocFiles(srcDir, destDir, overwrite) {
+        var copyFunction = overwrite ? subprocess.copyDirs : subprocess.copyDirsN;
+        this.docFiles.forEach(docFile => {
+            var src = path.join(srcDir, docFile);
+            if (fs.existsSync(src)) {
+                copyFunction(src, destDir);
+            }
+        })
+    }
+
+    copyDocsFromWorkingDirToTmpDir() {
+        this.copyDocFiles(config.wd, this.tempDir, true);
+    }
+
+    copyDocsFromTmpDirToWorkingDir() {
+        this.copyDocFiles(this.tempDir, config.wd, false);
+    }
+
+    addCommitPush() {
+        subprocess.exec(`git add .`);
+        subprocess.exec(`git commit -m "testing automated commit and push"`);
+        subprocess.exec(`git push`);
+    }
+}
+
 const fetchPages = () => {
-
-    subprocess.removeDirs('.ghpages-tmp');
-    subprocess.makeDirs('.ghpages-tmp');
-    var startDir = shell.pwd();
-    subprocess.changeDir('.ghpages-tmp');
-
-    log.log(`\nCloning 'gh-pages' branch into '${shell.pwd().stdout}'`);
-
-    subprocess.exec(`git clone --depth=1 --branch=gh-pages ${config.repoOrigin} .`);
-
-    if (fs.existsSync(config.branchPathBase)) {
-        subprocess.copyDirsN(config.branchPathBase, config.root);
-    }
-    if (fs.existsSync(config.docsRoot)) {
-        subprocess.copyDirsN(config.docsRoot, config.root);
-        subprocess.copyDirsN('openapi.*', config.root);
-    }
-
-    subprocess.changeDir(startDir);
-    subprocess.removeDirs('.ghpages-tmp');
+    var localGhPagesBranch = new LocalGithubPagesBranch();
+    localGhPagesBranch.createEnterTempDir();
+    localGhPagesBranch.copyDocsFromTmpDirToWorkingDir();
+    localGhPagesBranch.leaveRemoveTempDir();
 };
 
 const pushToPages = () => {
-    
-    if (fs.existsSync(config.branchPathBase)) {
-        subprocess(exec, `git add ${config.branchPathBase}`).runAndAssert();
-    }
-    if (fs.existsSync(config.docsRoot)) {
-        subprocess(exec, `git add ${config.docsRoot}`).runAndAssert();
-    }
-    if (fs.existsSync("./openapi.json")) {
-        subprocess(exec, `git add ./openapi.json`).runAndAssert();
-    }
-    if (fs.existsSync("./openapi.yaml")) {
-        subprocess(exec, `git add ./openapi.yaml`).runAndAssert();
-    }
-
-    subprocess(exec, `git stash save`).runAndAssert();
-    subprocess(exec, `git checkout gh-pages`).runAndAssert();
-    subprocess(exec, `git pull`).runAndAssert();
-    subprocess(exec, `git checkout stash -- .`).runAndAssert();
-    // subprocess(exec, `git commit -m "testing automated push"`).runAndAssert();
-    // subprocess(exec, `git push`).runAndAssert();
+    var localGhPagesBranch = new LocalGithubPagesBranch();
+    localGhPagesBranch.createEnterTempDir();
+    localGhPagesBranch.copyDocsFromWorkingDirToTmpDir();
+    localGhPagesBranch.addCommitPush();
+    localGhPagesBranch.leaveRemoveTempDir();
 }
 
 export { fetchPages, pushToPages };
